97cada6b209258fb2f7811c5189f21d87d06424c
Lucas Almeida <lucalmr12@gmail.com>
2023-07-31 20:45:02 -0300

    Adicionar arquivos de projeto.

---
+++ Excecoes001/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Excecoes001
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try { 
+            int n1 = int.Parse(Console.ReadLine());
+            int n2 = int.Parse(Console.ReadLine());
+
+
+            int resultado = n1 / n2;
+            Console.WriteLine(resultado);
+            }
+            catch (DivideByZeroException ) { 
+                Console.WriteLine("Não e possivel dividir o valor por zero! ");
+            
+            }
+            catch (FormatException)
+            {//não sendo digitado um valor inteiro acima ( exception Format)
+                Console.WriteLine("Você digitou um caractere não permito!!");
+            }
+            
+            
+            finally {
+                Console.WriteLine();
+
+                Console.WriteLine("Obrigado pela participação no teste!!");
+            
+            }
+            Console.ReadKey();
+
+        }
+        
+    }
+}
